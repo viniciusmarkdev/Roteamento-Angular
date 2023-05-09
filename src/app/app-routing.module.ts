bfe38dc770e83738a0d39d5a9431eefe2b4a6800
Marcos Vinicius <contato.marcoscandido@gmail.com>
2023-05-09 17:24:12 -0300

    lazy-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,29 +4,33 @@ import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
 
 
 
-const routes: Routes = [
+  const routes: Routes = [
 
 
 
-  { path: "primeira-pagina", component: PrimeiraPaginaComponent },
-  { path: "segunda-pagina ", component: SegundaPaginaComponent },
-  { path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
-  { path: "pagina-com-parametros/:id" , component: PaginaComParametrosComponent },
-  { path: "**", component: PaginaNaoEncontradaComponent }
- 
-
+    { path: "primeira-pagina", component: PrimeiraPaginaComponent },
+    { path: "segunda-pagina", component: SegundaPaginaComponent },
+    { path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
+    { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent},
+    { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+    { path: "pagina-protegida", component:PaginaProtegidaComponent},
+    { path: "**", component: PaginaNaoEncontradaComponent }
+    
   
 
-]
+    
+
+  ]
 
 
 
-@NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  @NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ src/app/lazy-loading/lazy-loading.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy-loading',
+  templateUrl: './lazy-loading.component.html',
+  styleUrls: ['./lazy-loading.component.css']
+})
+export class LazyLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    
+  }
+
+}
